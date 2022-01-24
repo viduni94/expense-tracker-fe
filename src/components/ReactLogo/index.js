e4e635f37a01dd8dd1a53521a6150597fd034e79
viduni94 <viduni.ushanka@gmail.com>
2022-01-24 11:13:46 +0530

    Remove unused component

--- src/components/ReactLogo/index.js
+++
@@ -1,9 +0,0 @@
-import { memo } from 'react';
-import Logo from 'assets/icons/logo.svg';
-import styles from './reactLogo.module.scss';
-
-const ReactLogo = ({ ...props }) => {
-  return <img className={styles.logo} src={Logo} alt="Logo" {...props} />;
-};
-
-export default memo(ReactLogo);
